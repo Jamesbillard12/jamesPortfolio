7904a4dc7ca83cfdbd4f33e2f08dd3f56f0c12b2
jamesbillard <jamesbillard12@hotmail.com>
2017-04-24 23:30:05 -0700

    Edu feature not working yet, but I am able to filter it so yatzee

--- starter-code/public/js/aboutApp.js
+++ starter-code/public/js/aboutApp.js
@@ -1,8 +1,20 @@
 
 
 var resumeArray = [];
+var resumeEduArray = [];
 
 function Resume(aboutRawDataObj){
+  this.company = aboutRawDataObj.company;
+  this.position= aboutRawDataObj.position;
+  this.cityPlace = aboutRawDataObj.cityPlace;
+  this.imgURL = aboutRawDataObj.imgURL;
+  this.employmentPeriodStart = aboutRawDataObj.employmentPeriodStart;
+  this.employmentPeriodEnd = aboutRawDataObj.employmentPeriodEnd;
+  this.description = aboutRawDataObj.description;
+  this.acategory = aboutRawDataObj.acategory;
+}
+
+function Edu(aboutRawDataObj){
   this.company = aboutRawDataObj.company;
   this.position= aboutRawDataObj.position;
   this.cityPlace = aboutRawDataObj.cityPlace;
@@ -23,16 +35,35 @@ Resume.prototype.aboutToHtml = function() {
 
 if (localStorage.aboutRawData) {
   var about = JSON.parse(localStorage.aboutRawData);
+  console.log(about);
 
-  about.forEach(function(aboutObject) {
-    resumeArray.push(new Resume(aboutObject));
-  });
-  resumeArray.forEach(function(about) {
-    $('#abouttodom').append(about.aboutToHtml());
-  });
-  aboutView.aboutPopulateFilter();
-  aboutView.aboutHandleCategoryFilter();
-  aboutView.setTeasers();
+
+  if ($(about).filter(function (i,n){return n.acategory==='Work Experience'})) {
+    about.forEach(function(aboutObject) {
+      resumeArray.push(new Resume(aboutObject));
+    });
+    resumeArray.forEach(function(about) {
+      $('#abouttodom').append(about.aboutToHtml());
+    });
+    aboutView.aboutPopulateFilter();
+    aboutView.aboutHandleCategoryFilter();
+    aboutView.setTeasers();
+  }
+  if ($(about).filter(function (i,n){return n.acategory==='Education'})) {
+    var about = $(about).filter(function (i,n){return n.acategory==='Education'});
+    console.log(about);
+    about.each(function(eduObject) {
+      resumeEduArray.push(new Edu(eduObject));
+    });
+    resumeEduArray.forEach(function(about) {
+      $('#aboutedutodom').append(about.aboutToHtml());
+    });
+    aboutView.aboutPopulateFilter();
+    aboutView.aboutHandleCategoryFilter();
+    aboutView.setTeasers();
+  }
+
+
 
 }else {
   $(function(){
