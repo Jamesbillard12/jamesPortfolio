88cdc7756df9dc2a962a00c9aaaca1d9b1d11fe2
jamesbillard <jamesbillard12@hotmail.com>
2017-04-25 23:53:53 -0700

    Added new append code to ajax request. added SMU and UBC

--- starter-code/public/js/aboutApp.js
+++ starter-code/public/js/aboutApp.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 var resumeArray = [];
 var resumeEduArray = [];
@@ -15,12 +15,11 @@ function Resume(aboutRawDataObj){
 }
 
 function Edu(aboutRawDataObj){
-  this.company = aboutRawDataObj.company;
-  this.position= aboutRawDataObj.position;
+  this.degree = aboutRawDataObj.degree;
+  this.school= aboutRawDataObj.school;
   this.cityPlace = aboutRawDataObj.cityPlace;
   this.imgURL = aboutRawDataObj.imgURL;
-  this.employmentPeriodStart = aboutRawDataObj.employmentPeriodStart;
-  this.employmentPeriodEnd = aboutRawDataObj.employmentPeriodEnd;
+  this.finished = aboutRawDataObj.finished;
   this.description = aboutRawDataObj.description;
   this.acategory = aboutRawDataObj.acategory;
 }
@@ -32,10 +31,10 @@ Resume.prototype.aboutToHtml = function() {
   return aboutTemplate(this);
 };
 
-Edu.prototype.aboutToHtml = function() {
-  var aboutSource = $('#aboutTemplate').html();
-  var aboutTemplate = Handlebars.compile(aboutSource);
-  return aboutTemplate(this);
+Edu.prototype.aboutEduToHtml = function() {
+  var aboutEduSource = $('#aboutEduTemplate').html();
+  var aboutEduTemplate = Handlebars.compile(aboutEduSource);
+  return aboutEduTemplate(this);
 };
 
 
@@ -44,30 +43,28 @@ if (localStorage.aboutRawData) {
   console.log(about);
 
 
-  if ($(about).filter(function (i,n){return n.acategory==='Work Experience'})) {
-    about.forEach(function(aboutObject) {
+  if ($(about).filter(function (i,n){
+    return n.acategory==='Work Experience'})) {
+
+    var getExperience = $(about).filter(function (i,n){return n.acategory==='Work Experience'});
+    var renderExpToVilJs = jQuery.makeArray( getExperience );
+    renderExpToVilJs.forEach(function(aboutObject) {
       resumeArray.push(new Resume(aboutObject));
     });
     resumeArray.forEach(function(about) {
       $('#abouttodom').append(about.aboutToHtml());
     });
   }
+
   if ($(about).filter(function (i,n){
     return n.acategory==='Education'})) {
-      
       var getEduc = $(about).filter(function (i,n){return n.acategory==='Education'});
-
-      console.log(getEduc);
-
-      var dog = jQuery.makeArray( getEduc );
-
-      console.log(dog);
-
-      dog.forEach(function(eduObject) {
+      var renderEduToVilJs = jQuery.makeArray( getEduc );
+      renderEduToVilJs.forEach(function(eduObject) {
         resumeEduArray.push(new Edu(eduObject));
       });
       resumeEduArray.forEach(function(about) {
-        $('#aboutedutodom').append(about.aboutToHtml());
+        $('#aboutedutodom').append(about.aboutEduToHtml());
       });
     }
     aboutView.aboutPopulateFilter();
@@ -85,12 +82,30 @@ if (localStorage.aboutRawData) {
         localStorage.setItem('aboutRawData', JSON.stringify(data));
         var about = JSON.parse(localStorage.aboutRawData);
 
-        about.forEach(function(aboutObject) {
-          resumeArray.push(new Resume(aboutObject));
-        });
-        resumeArray.forEach(function(about) {
-          $('#abouttodom').append(about.aboutToHtml());
-        });
+        if ($(about).filter(function (i,n){
+          return n.acategory==='Work Experience'})) {
+
+          var getExperience = $(about).filter(function (i,n){return n.acategory==='Work Experience'});
+          var renderExpToVilJs = jQuery.makeArray( getExperience );
+          renderExpToVilJs.forEach(function(aboutObject) {
+            resumeArray.push(new Resume(aboutObject));
+          });
+          resumeArray.forEach(function(about) {
+            $('#abouttodom').append(about.aboutToHtml());
+          });
+        }
+
+        if ($(about).filter(function (i,n){
+          return n.acategory==='Education'})) {
+            var getEduc = $(about).filter(function (i,n){return n.acategory==='Education'});
+            var renderEduToVilJs = jQuery.makeArray( getEduc );
+            renderEduToVilJs.forEach(function(eduObject) {
+              resumeEduArray.push(new Edu(eduObject));
+            });
+            resumeEduArray.forEach(function(about) {
+              $('#aboutedutodom').append(about.aboutEduToHtml());
+            });
+          }
         aboutView.aboutPopulateFilter();
         aboutView.aboutHandleCategoryFilter();
         aboutView.setTeasers();
