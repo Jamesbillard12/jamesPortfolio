dae53f483bd813c4fb6d2e276501f2458a17d6c0
jamesbillard <jamesbillard12@hotmail.com>
2017-05-08 01:48:46 -0700

    somehow it started to work

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const requestProxy = require('express-request-proxy');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
