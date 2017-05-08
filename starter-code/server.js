e0cb656fa7ef2f93c1d343579709ce56fef70034
jamesbillard <jamesbillard12@hotmail.com>
2017-05-07 23:30:42 -0700

    made changes for heroku

--- starter-code/server.js
+++ starter-code/server.js
@@ -5,6 +5,8 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+const constring = process.env.PORT;
+
 app.use(express.static(__dirname + '/public'))
 function proxyGitHub(request, response) {
   console.log('Routing GitHub request for', request.params[0]);
